48e8080dc852c1afeb92e118cc7d325fd176c81f
Alair Fonseca <alairfonseca13@gmail.com>
2022-03-06 01:49:42 -0300

    Time: 74 ms (75.45%), Space: 14 MB (71.85%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -11,11 +11,10 @@ class Solution:
         for (i, ch) in enumerate(s):
             if not ch in visited or visited[ch] < subs_start:
                 subs_end = i + 1
-                print(longest_subs_size, subs_end, subs_start)
                 longest_subs_size = max(longest_subs_size, subs_end - subs_start)
             else:
-                print(ch, visited[ch])
                 subs_start = visited[ch] + 1
+
             visited[ch] = i
         
         return longest_subs_size
