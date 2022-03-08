6af7448ce2d1dc6074b29448b03f23060ca4430b
Alair Fonseca <alairfonseca13@gmail.com>
2022-03-08 12:47:00 -0300

    Time: 36 ms (87.23%), Space: 14.1 MB (54.02%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,37 @@
+class Solution:
+    def clamp(self, x):
+        minimum = -2 ** 31
+        maximum = 2 ** 31 - 1
+        
+        if x > maximum:
+            print(">")
+            return maximum
+        if x < minimum:
+            print("<")
+            return minimum
+        print("x", x)
+        return x
+    
+    def myAtoi(self, s: str) -> int:
+        result = 0
+        s = s.lstrip()
+        symbol = 1
+        
+        if not s:
+            return 0
+        
+        if s[0] == "-" or s[0] == "+":
+            if s[0] == "-":
+                symbol = -1
+            
+            s = s[1:]
+        
+        for ch in s:
+            print(ch)
+            if not ch.isdigit():
+                break;
+            
+            result = (result * 10) + int(ch)
+            print(result)
+        
+        return self.clamp(result * symbol)
