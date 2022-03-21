6908fc30d564422bafa62d442ececba9a1f8c3a2
Alair Fonseca <alairfonseca13@gmail.com>
2022-03-21 20:37:49 -0300

    Time: 39 ms (77.67%), Space: 14.1 MB (12.28%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        result = ""
+        longest_prefix = ""
+        
+        for i in range(len(strs[0])):
+            add_char = True
+            
+            for j in range(1, len(strs)):
+                if i < len(strs[j]):
+                    if strs[j][i] != strs[0][i]:
+                        add_char = False
+                else:
+                    add_char = False
+                    break
+
+            if add_char:
+                longest_prefix += strs[0][i]
+            else:
+                break
+        
+        return longest_prefix
